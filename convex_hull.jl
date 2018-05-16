04207f6918a66673b737b545b1b47b8f9ccbfa79
MrShemek <przemyslaw@mirota.pl>
2018-05-16 22:54:25 +0200

    (tc4) Passing test

--- convex_hull.jl
+++ convex_hull.jl
@@ -19,3 +19,12 @@ end
 function counter_clockwise(p1, p2, p3)
   return ((p2[1] - p1[1]) * (p3[2] - p1[2])) - ((p2[2] - p1[2]) * (p3[1] - p1[1]))
 end
+
+function generate_points(number_of_points, range_min, range_max)
+  points = []
+  for i = 1:number_of_points
+    generated_point = (rand(range_min:range_max), rand(range_min:range_max))
+    push!(points, generated_point)
+  end
+  return points
+end  
